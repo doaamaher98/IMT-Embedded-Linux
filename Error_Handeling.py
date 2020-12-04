2b43ed91a28cbad1594a1511b36270d1a6b7c8f5
Doaa Maher <52929402+doaamaher98@users.noreply.github.com>
2020-12-04 21:41:07 +0200

    Update Error_Handeling.py

--- Error_Handeling.py
+++ Error_Handeling.py
@@ -1,9 +1,9 @@
-##########################################################
-###   Author      : Doaa Maher               		  ####
-###   File 		  : Checking if file was existed      ####
-###   Date        : 4 Dec 2020                		  ####
-###   Version     : 1.0                               ####
-##########################################################
+###########################################################
+###   Author      : Doaa Maher               	       ####
+###   File 	   : Checking if file was existed      ####
+###   Date        : 4 Dec 2020                	       ####
+###   Version     : 1.0                                ####
+###########################################################
 try :
 	file = open("missingfile.txt" , "r")
 except FileNotFoundError :
