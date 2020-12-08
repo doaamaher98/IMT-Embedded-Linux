a46c005fb8b96039fe9dc0c2427019c6121f1d55
Doaa Maher <52929402+doaamaher98@users.noreply.github.com>
2020-12-08 16:06:50 +0200

    Add files via upload

---
+++ GUI_ImgButton.py
@@ -0,0 +1,25 @@
+######################################################
+###   Author      : Doaa Maher              	  ####
+###   File 		  : GUI Button Image			  ####
+###   Date        : 8 Dec 2020                	  ####
+###   Version     : 1.0                   	      ####
+######################################################
+from tkinter import *
+
+# Tkinter window creation
+root = Tk()
+
+# Adding the widgets and packing it on top
+Label(root, text = "ImageButton", font = ("Arial", 20)).pack(side = TOP)
+
+# Creating PhotoImage object to use image
+photo = PhotoImage(file = "img.png") 
+
+# Return a new Photoimage based on the same image 
+# Subsample to scale the photo
+photo = photo.subsample(1,1)
+
+# Set image to overwrite the button and pack it on top
+Button(root , text = "Click here!" , image = photo).pack(side = TOP)
+
+mainloop()
