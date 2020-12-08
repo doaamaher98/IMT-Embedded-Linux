38a23aff6d33f394609f95d41d5b292ae8ef9ccf
Your Name <you@example.com>
2020-12-08 15:26:08 +0200

    Updated

---
+++ GUI_ButtonPressing.py
@@ -0,0 +1,21 @@
+##################################################
+###   Author      : Doaa Maher                ####
+###   File 		  : Mutable and Immutable obj ####
+###   Date        : 8 Dec 2020                ####
+###   Version     : 1.0                       ####
+##################################################
+from tkinter import *
+
+root = Tk ()
+root.title("Welcome to Tkinter")
+root.geometry('400x100')
+
+# A function to track the button pressings
+def ButtonFunction () :
+	print("Button is Pressed!")
+
+button = Button(root, text="Click me!", bd = "5", command = ButtonFunction)
+button.pack(side = "top")
+
+
+root.mainloop()
